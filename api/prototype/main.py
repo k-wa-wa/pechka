ed4d82e781cfd3cf575cdcfb3c06cadc749b5a9b
watanabe <sek.ohei.w0822@icloud.com>
2024-08-24 18:57:15 +0900

    initial commit

---
+++ api/prototype/main.py
@@ -0,0 +1,113 @@
+from pydantic import BaseModel
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse, JSONResponse
+from langchain_core.messages import HumanMessage
+from langgraph.prebuilt import create_react_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.tools import DuckDuckGoSearchRun
+from langgraph.checkpoint.memory import MemorySaver
+
+
+memory = MemorySaver()
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0)
+tools = [DuckDuckGoSearchRun()]
+agent_executor = create_react_agent(model, tools, checkpointer=memory)
+
+
+async def chat(message: str):
+    async for event in agent_executor.astream_events(
+        {"messages": [HumanMessage(content=message)]},
+        {"configurable": {"thread_id": "abc123"}},
+        version="v1"
+    ):
+        kind = event["event"]
+        if kind == "on_chain_start":
+            if (
+                event["name"] == "Agent"
+            ):  # Was assigned when creating the agent with `.with_config({"run_name": "Agent"})`
+                print(
+                    f"Starting agent: {event['name']} with input: {event['data'].get('input')}"
+                )
+        elif kind == "on_chain_end":
+            if (
+                event["name"] == "Agent"
+            ):  # Was assigned when creating the agent with `.with_config({"run_name": "Agent"})`
+                print()
+                print("--")
+                print(
+                    f"Done agent: {event['name']} with output: {event['data'].get('output')['output']}"
+                )
+        if kind == "on_chat_model_stream":
+            content = event["data"]["chunk"].content
+            if content:
+                # Empty content in the context of OpenAI means
+                # that the model is asking for a tool to be invoked.
+                # So we only print non-empty content
+                print(content, end="|")
+                yield content
+        elif kind == "on_tool_start":
+            print("--")
+            print(
+                f"Starting tool: {event['name']} with inputs: {event['data'].get('input')}"
+            )
+        elif kind == "on_tool_end":
+            print(f"Done tool: {event['name']}")
+            print(f"Tool output was: {event['data'].get('output')}")
+            print("--")
+
+
+app = FastAPI()
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class PostChatReq(BaseModel):
+    model: str
+    messages: list[Message]
+
+
+class PostChatRes(BaseModel):
+    model: str
+    created_at: str
+    message: Message
+    done: bool
+
+
+@app.post("/api/chat")
+async def post_chat(req: Request):
+    reqBody = PostChatReq.model_validate_json(await req.body())
+
+    async def generate_chat():
+        message = "\n".join([m.content for m in reqBody.messages])
+        async for m in chat(message):
+            res = PostChatRes(model="", created_at="", message=Message(role="assistant", content=m), done=False)
+            yield f"{res.model_dump_json()}\n\n"
+        res = PostChatRes(model="", created_at="", message=Message(role="model", content=""), done=True)
+        yield f"{res.model_dump_json()}\n\n"
+
+    return StreamingResponse(content=generate_chat(), media_type="application/x-ndjson")
+
+
+@app.get("/api/tags")
+async def get_tags():
+    return JSONResponse(content={
+        "models": [
+            {
+                "name": "llama3:latest",
+                "model": "llama3:latest",
+                "modified_at": "2023-12-07T09:32:18.757212583-08:00",
+                "size": 3825819519,
+                "digest": "fe938a131f40e6f6d40083c9f0f430a515233eb2edaa6d72eb85c50d64f2300e",
+                "details": {
+                    "format": "gguf",
+                    "family": "llama",
+                    "families": None,
+                    "parameter_size": "7B",
+                    "quantization_level": "Q4_0"
+                }
+            }
+        ]
+    })
