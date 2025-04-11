cc6d6682cf963568235aba0064af6eb028716af6
watanabe <sek.ohei.w0822@icloud.com>
2025-04-12 04:36:33 +0900

    update: envの指定を整理

--- file-server/ui/vite.config.ts
+++ file-server/ui/vite.config.ts
@@ -2,8 +2,6 @@ import { vitePlugin as remix } from "@remix-run/dev"
 import { defineConfig } from "vite"
 import path from 'path';
 
-const API_URL = "http://localhost:8000"
-
 export default defineConfig({
   plugins: [remix()],
   resolve: {
