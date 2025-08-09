d8fb3791a731c780e4e48a74a8933c626c9e7c83
watanabe <sek.ohei.w0822@icloud.com>
2025-08-10 02:24:26 +0900

    update: request-idの伝播

--- file-server/ui/app/routes/_index.tsx
+++ file-server/ui/app/routes/_index.tsx
@@ -2,9 +2,15 @@ import { Anchor, Breadcrumbs, Flex, Stack, Title } from "@mantine/core"
 import { Playlist, Video } from "@/src/types"
 import Videos from "@/src/components/Videos"
 import { Link, useLoaderData } from "@remix-run/react"
+import { LoaderFunctionArgs } from "@remix-run/node"
 
-export const loader = async () => {
-  const data = await fetch(`${process.env.API_URL}/api/playlists`)
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const requestId = request.headers.get("x-request-id") || `ui_${crypto.randomUUID()}`
+  const data = await fetch(`${process.env.API_URL}/api/playlists`, {
+    headers: {
+      "x-request-id": requestId,
+    },
+  })
   const playlists: Playlist[] = await data.json()
   return playlists
 }
