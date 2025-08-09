b05d92f05f95b8b9f8374239e2c5cf289ab5c1c7
watanabe <sek.ohei.w0822@icloud.com>
2025-08-09 23:58:30 +0900

    change: uiログフォーマット変更(json化)

--- file-server/ui/server.js
+++ file-server/ui/server.js
@@ -3,9 +3,25 @@ import morgan from "morgan"
 import express from "express"
 const app = express()
 
-const combinedFormat = `:remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"`
-const logFormat = `${combinedFormat} ":req[x-request-id]"`
-app.use(morgan(logFormat))
+morgan.token('jsonLogFormat', (req, res) => {
+  return JSON.stringify({
+    timestamp: new Date().toISOString(),
+    remote_ip: req.ip,
+    request_id: req.get('x-request-id'),
+    host: req.get('host'),
+    request: {
+      method: req.method,
+      path: req.originalUrl,
+      protocol: req.protocol,
+    },
+    status: res.statusCode,
+    bytes: res.get('Content-Length'),
+    referer: req.get('referer'),
+    user_agent: req.get('user-agent'),
+    request_time_ms: morgan['response-time'](req, res),
+  });
+});
+app.use(morgan(':jsonLogFormat'));
 
 app.use(
   "/assets",
