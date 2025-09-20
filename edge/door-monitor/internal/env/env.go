cbc89893e285c5e7d80cb25e0f4815e1e2fad6e7
watanabe <sek.ohei.w0822@icloud.com>
2025-09-21 04:42:53 +0900

    delete: rustに置き換えたため削除

--- edge/door-monitor/internal/env/env.go
+++
@@ -1,9 +0,0 @@
-package env
-
-import "os"
-
-var (
-	SENSOR_MAC        = os.Getenv("SENSOR_MAC")
-	DATABASE_URL      = os.Getenv("DATABASE_URL")
-	SLACK_WEBHOOK_URL = os.Getenv("SLACK_WEBHOOK_URL")
-)
